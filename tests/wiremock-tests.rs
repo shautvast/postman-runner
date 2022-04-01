9459a354545d4f7a86938055a95c683388ca3d01
Sander Hautvast <sander.hautvast@ing.com>
2022-04-01 17:42:00 +0200

    removed deceitful comments

--- tests/wiremock-tests.rs
+++ tests/wiremock-tests.rs
@@ -5,17 +5,13 @@ use postman_runner::{collection, environment};
 
 #[tokio::test]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>>{
-    // Start a background HTTP server on a random local port
     let listener = std::net::TcpListener::bind("127.0.0.1:8080").unwrap();
     let expected_server_address = listener
         .local_addr()
         .expect("Failed to get server address.");
 
-    // // Act
     let mock_server = MockServer::builder().listener(listener).start().await;
     
-    // Arrange the behaviour of the MockServer adding a Mock:
-    // when it receives a GET request on '/hello' it will respond with a 200.
     Mock::given(method("GET"))
         .and(path("/headers"))
         .respond_with(ResponseTemplate::new(200))
